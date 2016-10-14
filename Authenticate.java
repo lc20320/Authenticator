3f7ff54169cc6990d508f95bf23967bd6b3db179
Luis Cervantes <Luis Cervantes>
2016-10-14 15:24:30 -0400

    initial commit

---
+++ Authenticate.java
@@ -0,0 +1,26 @@
+import javax.swing.JOptionPane;
+
+/**
+*
+* Luis Cervantes
+* This program authenticates username and password
+*
+*/
+
+public class Authenticate {
+
+	public static void main(String [] args) {
+		String username = "User";
+		String pass = "Pass123";
+
+		String userinput = JOptionPane.showInputDialog("Enter Username: ");
+		String passinput = JOptionPane.showInputDialog("Enter Password: ");
+
+		if (userinput.equals(username) && passinput.equals(pass)) {
+			JOptionPane.showMessageDialog(null, "Welcome Username!");
+
+		} else {
+			JOptionPane.showMessageDialog(null, "Invalid Username or Password");
+		}
+	}
+}
